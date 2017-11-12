6eeed67a581d21f66e5b71bd8417e091ff31e3b9
rahul <rahulsingh2240@gmail.com>
2017-11-12 23:03:07 +0530

    working url shortner

--- url/UrlController.js
+++ url/UrlController.js
@@ -13,7 +13,7 @@ router.post('/',function(req, res){
 
 	function(err, url){
 		if (err) return res.status(500).send("There is a problem");
-		res.status(200).send(url);
+		res.status(200).send(url.id);
 	});
 });
 
@@ -21,8 +21,7 @@ router.get('/:id', function(req, res){
 
 	Url.findById(req.params.id, {_id: false,name: true}, function(err, url){
 		if (err) return res.status(500).send("There is a problem");
-		res.status(200).send(url.name);
-
+		res.status(200).redirect(url.name);
 	});
 
 });
